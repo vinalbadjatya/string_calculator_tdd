9e894ecbad84066d33891423eadef610cbd4a08d
Vinal Badjatya <vinaljain2204@gmail.com>
2025-04-12 21:57:22 +0530

    Refactores test cases

--- app/spec/models/string_calculator_spec.rb
+++ app/spec/models/string_calculator_spec.rb
@@ -1,39 +1,33 @@
 require_relative '../../models/string_calculator'
+
 RSpec.describe StringCalculator do
   describe "#add" do
     it "returns 0 for an empty string" do
-      calculator = StringCalculator.new
-      expect(calculator.add("")).to eq(0)
+      expect(StringCalculator.new.add("")).to eq(0)
     end
 
     it "returns the number itself for a single number" do
-      calculator = StringCalculator.new
-      expect(calculator.add("1")).to eq(1)
+      expect(StringCalculator.new.add("1")).to eq(1)
     end
 
     it "returns the sum of two numbers" do
-      calculator = StringCalculator.new
-      expect(calculator.add("1,2")).to eq(3)
+      expect(StringCalculator.new.add("1,2")).to eq(3)
     end
 
-    it "returns the sum of numbers separated by new lines" do
-      calculator = StringCalculator.new
-      expect(calculator.add("1\n2,3")).to eq(6)
+    it "handles numbers separated by new lines" do
+      expect(StringCalculator.new.add("1\n2,3")).to eq(6)
     end
 
     it "supports custom delimiters" do
-      calculator = StringCalculator.new
-      expect(calculator.add("//;\n1;2")).to eq(3)
+      expect(StringCalculator.new.add("//;\n1;2")).to eq(3)
     end
 
-    it "throws an error when negative numbers are provided" do
-      calculator = StringCalculator.new
-      expect { calculator.add("1,-2,3") }.to raise_error("negatives not allowed: -2")
+    it "raises an error when a single negative number is provided" do
+      expect { StringCalculator.new.add("1,-2,3") }.to raise_error(RuntimeError, "negatives not allowed: -2")
     end
 
-    it "throws an error when multiple negative numbers are provided" do
-      calculator = StringCalculator.new
-      expect { calculator.add("1,-2,-3") }.to raise_error("negatives not allowed: -2, -3")
+    it "raises an error listing all negative numbers when multiple are provided" do
+      expect { StringCalculator.new.add("1,-2,-3") }.to raise_error(RuntimeError, "negatives not allowed: -2, -3")
     end
   end
 end
