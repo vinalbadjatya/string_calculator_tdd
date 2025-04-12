36fd2eb51849cb3ae92cd7740bb368e968c99e30
Vinal Badjatya <vinaljain2204@gmail.com>
2025-04-12 21:12:11 +0530

    Write test for single number

--- app/spec/models/string_calculator_spec.rb
+++ app/spec/models/string_calculator_spec.rb
@@ -5,5 +5,10 @@ RSpec.describe StringCalculator do
       calculator = StringCalculator.new
       expect(calculator.add("")).to eq(0)
     end
+
+    it "returns the number itself for a single number" do
+      calculator = StringCalculator.new
+      expect(calculator.add("1")).to eq(1)
+    end
   end
 end
