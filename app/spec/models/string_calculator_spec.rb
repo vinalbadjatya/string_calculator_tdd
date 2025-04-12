da144ec500a2f55a6e7b32d4a1e3034ba0106092
Vinal Badjatya <vinaljain2204@gmail.com>
2025-04-12 21:33:01 +0530

    Write test for numbers with newlines

--- app/spec/models/string_calculator_spec.rb
+++ app/spec/models/string_calculator_spec.rb
@@ -15,5 +15,10 @@ RSpec.describe StringCalculator do
       calculator = StringCalculator.new
       expect(calculator.add("1,2")).to eq(3)
     end
+
+    it 'returns the sum of numbers separated by new lines' do
+      calculator = StringCalculator.new
+      expect(calculator.add("1\n2,3")).to eq(6)
+    end
   end
 end
