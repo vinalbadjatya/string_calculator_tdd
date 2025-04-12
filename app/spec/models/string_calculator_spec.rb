a77932507c439eb352b57343b83b6cb3e2d3c806
Vinal Badjatya <vinaljain2204@gmail.com>
2025-04-12 21:35:10 +0530

    Write test for custom delimiter

--- app/spec/models/string_calculator_spec.rb
+++ app/spec/models/string_calculator_spec.rb
@@ -11,14 +11,19 @@ RSpec.describe StringCalculator do
       expect(calculator.add("1")).to eq(1)
     end
 
-    it 'returns the sum of two numbers' do
+    it "returns the sum of two numbers" do
       calculator = StringCalculator.new
       expect(calculator.add("1,2")).to eq(3)
     end
 
-    it 'returns the sum of numbers separated by new lines' do
+    it "returns the sum of numbers separated by new lines" do
       calculator = StringCalculator.new
       expect(calculator.add("1\n2,3")).to eq(6)
+    end
+
+    it "supports custom delimiters" do
+      calculator = StringCalculator.new
+      expect(calculator.add("//;\n1;2")).to eq(3)
     end
   end
 end
