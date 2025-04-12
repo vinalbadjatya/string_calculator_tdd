c10a136df3f61fba10d3aa25702fc0e4c9a3767b
Vinal Badjatya <vinaljain2204@gmail.com>
2025-04-12 21:42:35 +0530

    Make custom delimiter test pass

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -1,6 +1,15 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    numbers.split(/,|\n/).map(&:to_i).sum
+
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      delimiter = parts[0][2..-1]
+      numbers = parts[1]
+      numbers = numbers.split(delimiter).map(&:to_i).sum
+    else
+      numbers = numbers.split(/,|\n/).map(&:to_i).sum
+    end
+    numbers
   end
 end
